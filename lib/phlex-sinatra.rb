1377e8726de6ad1bf722c698716491721764dab9
Ben Pickles <spideryoung@gmail.com>
2023-04-22 14:54:49 +0100

    Automatically set the correct content type for Phlex::SVG views

--- lib/phlex-sinatra.rb
+++ lib/phlex-sinatra.rb
@@ -26,6 +26,7 @@ end
 module Sinatra
   module Templates
     def phlex(obj, content_type: nil)
+      content_type ||= :svg if obj.is_a?(Phlex::SVG)
       self.content_type(content_type) if content_type
       obj.call(view_context: self)
     end
--- spec/phlex/sinatra_spec.rb
+++ spec/phlex/sinatra_spec.rb
@@ -16,6 +16,12 @@ class LinkView < Phlex::HTML
   end
 end
 
+class SvgElem < Phlex::SVG
+  def template
+    svg { rect(width: 100, height: 100) }
+  end
+end
+
 class TestApp < Sinatra::Application
   set :environment, :test
 
@@ -25,6 +31,14 @@ class TestApp < Sinatra::Application
 
   get '/link' do
     phlex LinkView.new(params[:full])
+  end
+
+  get '/svg' do
+    phlex SvgElem.new
+  end
+
+  get '/svg/plain' do
+    phlex SvgElem.new, content_type: :text
   end
 
   get '/xml' do
@@ -79,6 +93,22 @@ RSpec.describe Phlex::Sinatra do
 
       expect(last_response.body).to eql('<p>foo</p>')
       expect(last_response.media_type).to eql('application/xml')
+    end
+  end
+
+  context 'with a Phlex::SVG view' do
+    it 'responds with the correct content type by default' do
+      get '/svg'
+
+      expect(last_response.body).to start_with('<svg><rect')
+      expect(last_response.media_type).to eql('image/svg+xml')
+    end
+
+    it 'can also specify a content type' do
+      get '/svg/plain'
+
+      expect(last_response.body).to start_with('<svg><rect')
+      expect(last_response.media_type).to eql('text/plain')
     end
   end
 end
